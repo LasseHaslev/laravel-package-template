621fc668a9b294d2675fd312e02203bf888c6584
Lasse <lasse@haslev.no>
2017-06-25 10:49:33 +0200

    Added correct path of date time string

--- bin/app.js
+++ bin/app.js
@@ -104,7 +104,7 @@ var changeFilenames = function changeFilenames( response ) {
         } ),
         new Promise( function( resolve, reject ) {
             var date = new Date();
-            fs.rename( response.folder + '/database/migrations/my_package.php', response.folder + '/database/migrations/' + date.getFullYear() + '_' + ("0" + (date.getMonth() + 1)).slice(-2) + '_' + ("0" + date.getDate()).slice(-2) + '_' + date.getTime().toString().substring( 0, 6 ) + '_create_' + response.data.package.name + '_table.php', function( error ) {
+            fs.rename( response.folder + '/database/migrations/my_package.php', response.folder + '/database/migrations/' + date.getFullYear() + '_' + ("0" + (date.getMonth() + 1)).slice(-2) + '_' + ("0" + date.getDate()).slice(-2) + '_' + date.getTime().toString().substring( date.getTime().toString().length - 6 ) + '_create_' + response.data.package.name + '_table.php', function( error ) {
                 if (error) {
                     console.error( error );
                     reject( error );
